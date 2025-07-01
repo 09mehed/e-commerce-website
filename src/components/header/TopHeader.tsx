b21a0ab621d5ef0013d330a3bb2bcc932d398818
09mehed <foysalalmamun558@gmail.com>
2025-07-01 09:13:28 +0600

    e-commerce website is started

---
+++ src/components/header/TopHeader.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Container from '../Container'
+
+const TopHeader = () => {
+  return (
+    <div className='bg-[#010f1c] text-gray-200'>
+        <Container className='py-1'>
+            <p>Express shipping on orders $570+</p>
+        </Container>
+    </div>
+  )
+}
+
+export default TopHeader
