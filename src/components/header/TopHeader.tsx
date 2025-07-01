35569dfc14bdf3dc0967ef49797ac00afcb8e810
09mehed <foysalalmamun558@gmail.com>
2025-07-01 09:30:20 +0600

    top header menu is ok

--- src/components/header/TopHeader.tsx
+++ src/components/header/TopHeader.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import Container from '../Container'
+import { FaTruckMoving } from "react-icons/fa";
+import { IoChevronDownSharp } from 'react-icons/io5';
 
 const TopHeader = () => {
   return (
     <div className='bg-[#010f1c] text-gray-200'>
-        <Container className='py-1'>
-            <p>Express shipping on orders $570+</p>
+        <Container className='flex justify-between items-center'>
+            <p className='w-full md:w-auto text-sm flex items-center justify-center md:justify-normal font-medium py-1'>
+                <FaTruckMoving className='text-[#ffb342] text-2xl mr-1' />Free Express shipping on orders $570+
+            </p>
+            <div className='hidden md:inline-flex items-center text-sm text-white'>
+                <p className='headerTopMenu'>English <IoChevronDownSharp className='mt-1'></IoChevronDownSharp> </p>
+                <p className='headerTopMenu'>USD <IoChevronDownSharp className='mt-1'></IoChevronDownSharp> </p>
+                <p className='headerTopMenu'>Settings <IoChevronDownSharp className='mt-1'></IoChevronDownSharp> </p>
+            </div>
         </Container>
     </div>
   )
