00074e8cb9b32044c4651e1cf23e1849f46cbb5b
09mehed <foysalalmamun558@gmail.com>
2025-07-23 00:20:51 +0600

    google setup is ok

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
-import FacebookProvider from "next-auth/providers/google"
+import FacebookProvider from "next-auth/providers/facebook"
 
 export const authOptions = {
     providers: [
